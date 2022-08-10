2d38984e3b8e24646453315c330b56ace3e7fcf5
Marwan Nakhaleh <marwan@Marwans-MacBook-Air.local>
2022-08-10 10:26:07 -0400

    [WIP] scenario 2 done, other than needing to rearrange some of the functions in requestTokens() and making FaucetDetails only usable as an inherited contract

---
+++ contracts/FaucetDetails.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract FaucetDetails {
+    mapping (address => uint256) numberOfTimesAddressHasUsedFaucet;
+    mapping(address => uint256) totalNumberOfTokensUserHasPulled;
+
+    function getNumberOfTokensPulledForAddress(address addy) public view returns (uint256) {
+        return totalNumberOfTokensUserHasPulled[addy];
+    }
+
+    function getNumberOfTimesAddressHasUsedFaucet(address addy) public view returns (uint256) {
+        return numberOfTimesAddressHasUsedFaucet[addy];
+    }
+
+    function getNumberOfTokensIHavePulled() public view returns (uint256) {
+        return totalNumberOfTokensUserHasPulled[msg.sender];
+    }
+
+    function getNumberOfTimesIHaveUsedFaucet() public view returns (uint256) {
+        return numberOfTimesAddressHasUsedFaucet[msg.sender];
+    }
+}
